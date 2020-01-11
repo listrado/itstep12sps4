15243e2c1f10111bed0253553ec501b384db2406
Aluno <Aluno@DESKTOP-8JIME8G>
2020-01-11 17:36:23 -0200

    INCLUIR PESSOA

---
+++ massaterapeuta/SQL/INCLUIR_PESSOA.sql
@@ -0,0 +1,26 @@
+--------------------------------------------------------
+--  Arquivo criado - Sábado-Janeiro-11-2020   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Procedure INCLUIR_PESSOA
+--------------------------------------------------------
+set define off;
+
+  CREATE OR REPLACE NONEDITIONABLE PROCEDURE "T12SPS4"."INCLUIR_PESSOA" (
+    vNOME VARCHAR2,
+    vEMAIL VARCHAR2,
+    vSENHA VARCHAR2)
+
+IS
+    vEXCEPTION EXCEPTION;
+    BEGIN
+    
+        INSERT INTO T12SPS4.PESSOA(ID, NOME, EMAIL, SENHA) VALUES(T12SPS4.SEC_PESSOA01.NEXTVAL, vNOME, vEMAIL, vSENHA);
+        
+    EXCEPTION
+      WHEN vEXCEPTION THEN
+        RAISE_APPLICATION_ERROR(-20999,'ATENÇCAO FALHA NA INCLUSAO DE PESSOA', FALSE);
+        
+END INCLUIR_PESSOA;
+
+/
